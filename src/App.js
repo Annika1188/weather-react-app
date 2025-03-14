674383359e34f912d5a9d5839f9324f953801f7d
Annika1188 <annikas_lane@live.com.au>
2025-03-14 15:18:33 +1100

    added footer and git hub link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WeatherApp from "./Weather";
+import Footer from "./Footer";
 import "./styles.css";
 function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Coded by{" "}
+        <a
+          href="https://github.com/Annika1188/weather-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Annika Scharkie
+        </a>
+      </p>
+    </footer>
+  );
+}
